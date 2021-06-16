d1c05d3b7bb6cc54082095175236e3025a256e1e
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-06-16 11:14:04 +0300

    create user model

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
 
 class Config:
-    pass
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jose:joseotis45@localhost/movie'
+    SECRET_KEY= os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+     
+    MAIL_SERVER= 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 class TestConfig(Config):
     pass
@@ -10,6 +19,9 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jose:joseotis45@localhost/movie'
+   
+
     DEBBUG=True
 
 config_options={
