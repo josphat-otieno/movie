bf10163df84f0026104aefa9e070f39810c6a6bc
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-16 08:36:30 +0300

    Add Function that gets the json response to our url request

--- app/requests.py
+++ app/requests.py
@@ -13,3 +13,17 @@ def configure_request(app):
     global api_key,base_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
+
+def get_movie(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movie_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movie_url) as url:
+        get_movies_data = url.read()
+        get_movies_data = json.loads(get_movies_data)
+
+        movie_results = None
+    
+    return movie_results
