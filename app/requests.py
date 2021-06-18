f30bae08359e1ca906a1a778f83a225f0d65d27d
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-18 16:10:35 +0300

    Add genre url

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-from .models import Movie
+from .models import Movie, Genres
 
 
 
@@ -8,11 +8,15 @@ from .models import Movie
 api_key = None
 # Getting the movie base url
 base_url = None
+genres_url = None
+genre_movies_url = None
 
 def configure_request(app):
-    global api_key,base_url
+    global api_key,base_url, genres_url, genre_movies_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
+    genres_url = app.config['GENRES_URL']
+    genre_movies_url = app.config['GENRE_MOVIES_URL']
 
 def get_movies(category):
     '''
@@ -91,4 +95,34 @@ def search_movie(movie_name):
 
 
     return search_movie_results
+
+
+def get_genres():
+    get_genres_url = genres_url.format(api_key)
+    with urllib.request.urlopen(get_genres_url) as url:
+        get_genres_data = url.read()
+        get_genres_response = json.loads(get_genres_data)
+        genres_results = None
+        if get_genres_response['genres']:
+            genres_results_list = get_genres_response['genres']
+            genres_results = process_genres_results(genres_results_list)
+    return genres_results
+def process_genres_results(genres_results_list):
+    genres_results = []
+    for genre_item in genres_results_list:
+        id = genre_item.get('id')
+        name = genre_item.get('name')
+        genre_object = Genres(id,name)
+        genres_results.append(genre_object)
+    return genres_results
+def get_genre_movies(id):
+    get_genre_movies_url = genre_movies_url.format(api_key,id)
+    with urllib.request.urlopen(get_genre_movies_url) as url:
+        genre_movies_data = url.read()
+        genre_movies_response = json.loads(genre_movies_data)
+        genre_movies_results = None
+        if genre_movies_response['results']:
+            genre_movies_list = genre_movies_response['results']
+            genre_movies_results = process_results(genre_movies_list)
+    return genre_movies_results
 
--- config.py
+++ config.py
@@ -1,31 +1,57 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jose:joseotis45@localhost/movie'
-    SECRET_KEY= os.environ.get('SECRET_KEY')
+    '''
+    General configuration parent class
+    '''
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    GENRES_URL ='https://api.themoviedb.org/3/genre/movie/list?api_key={}'
+    GENRE_MOVIES_URL = 'https://api.themoviedb.org/3/discover/movie?api_key={}&with_genres={}'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/movie'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-     
-    MAIL_SERVER= 'smtp.gmail.com'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-
-class TestConfig(Config):
-    pass
+     # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-    pass
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Ammoh@localhost/watchlist_test'
+
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jose:joseotis45@localhost/movie'
-   
+    '''
+    Development  configuration child class
 
-    DEBBUG=True
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/watchlist'
+    DEBUG = True
 
-config_options={
+config_options = {
     'development':DevConfig,
-    'production':ProdConfig, 
+    'production':ProdConfig,
     'test':TestConfig
-}
+    }
+    
